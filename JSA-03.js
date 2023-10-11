0e8d01eadee7397ee2547fc639a33d672cb5f3ef
trxuanha207 <147617828+trxuanha207@users.noreply.github.com>
2023-10-11 19:11:49 +0700

    Add files via upload

---
+++ JSA-03.js
@@ -0,0 +1,5 @@
+function test(){
+    let a = ["iphone" , "sansung" , "oppo" ,"vsmart"];
+    let sl = document.getElementById('soluong').value;
+    document.getElementById('lb') .innerHTML="hãng dt bạn chọn: "+a[sl];
+}
